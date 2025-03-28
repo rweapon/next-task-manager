400ff13650ea4751bf400c316c7ea2ad84ff39ba
rweapon <mrkasper67@gmail.com>
2025-03-28 18:49:57 +0300

    fix: taskform button text

--- app/components/TaskForm.tsx
+++ app/components/TaskForm.tsx
@@ -74,7 +74,7 @@ export function TaskForm({ onSubmit, initialValues }: Props) {
         type="submit"
         className="w-full rounded-md  px-4 py-2 button--filter hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:hover:bg-blue-600"
       >
-        Добавить задачу
+        {initialValues ? "Изменить задачу" : "Добавить задачу"}
       </button>
     </form>
   );
